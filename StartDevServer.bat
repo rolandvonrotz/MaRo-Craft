5319a482aa9a13f20a7ee21fa4f12586d00dde3f
Roland von Rotz <roland@vonrotz-beutter.ch>
2017-05-05 21:01:58 +0200

    Fix Start.bat

--- StartDevServer.bat
+++ StartDevServer.bat
@@ -1,5 +1,5 @@
 @echo off
-copy /Y target\maro-craft-1.1-SNAPSHOT.jar S:\Server\Games\Minecraft\Develop\plugins\
+copy /Y target\maro-craft-1.1.1-SNAPSHOT.jar S:\Server\Games\Minecraft\Develop\plugins\
 S:
 cd S:\Server\Games\Minecraft\Develop
 java -jar craftbukkit-1.11.2.jar
